4f26f8a64e17f520d651b04d2745aa26a1f799d5
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-02-13 23:36:53 +0800

    Initial Commit

---
+++ recipebook/ledger/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+'''
+    We're using master_context so we can just pull from it 
+    instead of copy/pasting full context blocks
+'''
+master_context = {
+        "recipes": [
+            {
+                "name": "Recipe 1",
+                "ingredients": [
+                    {
+                        "name": "tomato",
+                        "quantity": "3pcs"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1kg"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1L"
+                    },
+                    {
+                        "name": "sinigang mix",
+                        "quantity": "1 packet"
+                    }
+                ],
+                "link": "/recipe/1"
+            },
+            {
+                "name": "Recipe 2",
+                "ingredients": [
+                    {
+                        "name": "garlic",
+                        "quantity": "1 head"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "vinegar",
+                        "quantity": "1/2cup"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1 cup"
+                    },
+                    {
+                        "name": "salt",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "whole black peppers",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1 kilo"
+                    }
+                ],
+                "link": "/recipe/2"
+            }
+        ]
+    }
+
+def index(request):
+    return HttpResponse('Hello World!')
+
+def recipes_list(request):
+    ctx = master_context
+    return render(request, 'recipes_list.html', ctx)
+
+'''
+    We use the same html file since the only thing that changes
+    between the two different pages is the data
+'''
+def recipe_1(request):
+    ctx = master_context["recipes"][0]
+    return render(request, 'recipe_base.html', ctx)
+
+def recipe_2(request):
+    ctx = master_context["recipes"][1]
+    return render(request, 'recipe_base.html', ctx)
+
