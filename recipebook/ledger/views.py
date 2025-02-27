50984b886444821654e774c5fe670ed9417b0f39
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-02-27 23:13:30 +0800

    Changed model of RecipeDetailView to Recipeso ingredients are visible

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Recipe, RecipeIngredient
+from .models import Recipe
 
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'home/recipes_list.html'
 
 class RecipeDetailView(DetailView):
-    model = RecipeIngredient
+    model = Recipe
     template_name = 'home/recipe_base.html'
