9314f609e58ac41504767181751bd005697af14e
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-02-27 18:16:47 +0800

    Created Ingredients model & migrated them

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
