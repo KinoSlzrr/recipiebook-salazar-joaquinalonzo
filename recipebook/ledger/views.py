2d921b534206fc08413391f7b106bf27eaf1044b
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-03-10 18:09:23 +0800

    Limited recipe detail view to logged in users

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Recipe
 
@@ -9,6 +10,7 @@ class RecipeListView(ListView):
     template_name = 'home/recipes_list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'home/recipe_base.html'
+    redirect_field_name = 'accounts/login'
