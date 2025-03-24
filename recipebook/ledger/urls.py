67cd8f16bf88f15fbc540b888dc2711bf8392fe8
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-03-24 17:30:59 +0800

    Linked Add Recipe page in recipe list

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,7 +5,7 @@ from .views import RecipeListView, RecipeDetailView, RecipeCreateView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name="recipes-list"),
-    path('recipe/add', RecipeCreateView.as_view(), name="recipe-create"),
+    path('recipe/add', RecipeCreateView.as_view(), name="recipe-add"),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail")
 ]
 
