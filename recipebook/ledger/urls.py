c9da068e3b0abc7d83bcac4fdd3b0102a6a68d88
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-03-24 17:10:00 +0800

    Fixed typos in recipe urls

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,7 +5,7 @@ from .views import RecipeListView, RecipeDetailView, RecipeCreateView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name="recipes-list"),
-    path('recipes/create', RecipeCreateView.as_view(), name="recipe-create"),
+    path('recipe/add', RecipeCreateView.as_view(), name="recipe-create"),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail")
 ]
 
