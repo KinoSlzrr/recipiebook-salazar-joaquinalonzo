f7a1c2ec8145eb3825658b340b1ddcfdc6af2916
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-03-24 17:24:07 +0800

    Removed STATICFILES_DIRS since it's unnecessary

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -121,10 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR/'static'
-]
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'ledger:recipes-list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
